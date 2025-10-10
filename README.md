dd8ba735f33088b6429e39787a0397bbbb96bf59
Connor Black <ckb1997@hotmail.co.uk>
2025-10-10 19:41:22 +0100

    added server-side shell script, updated Readme, created example .env

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# priceprowler
-See if your area is gaining or losing property value
+# Price Prowler
+See if your area is gaining or losing property value.
+
+There's a server side element to this, getHousePrices.sh is on a VPS, runs 
+monthly via a CRON job, writes to MariaDB.
+
+You'll need to go to the UK gov website and generate your own URL for your local area and get the params you want.
+
+The go code runs locally, speaks to the db, and generates a report on local 
+house prices going up or down, perhaps some other info, we'll see.
---
+++ getHousePrices.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+
+DB_USER=""
+DB_PASS=""
+DB_NAME=""
+DB_HOST=""
+
+# Dates: previous month
+MIN_DATE=$(date -d "$(date +%Y-%m-01) -1 month" +%Y-%m-01)
+MAX_DATE=$(date -d "$(date +%Y-%m-01) -1 day" +%Y-%m-%d)
+
+# for example: https://landregistry.data.gov.uk/app/ppd/
+URL="You can get this from UK gov land registry"
+
+TMP_FILE="./ppd_last_month.csv"
+
+echo "Downloading PPD data for $MIN_DATE to $MAX_DATE..."
+curl -k -v -s -o "$TMP_FILE" "$URL"
+
+echo "writing to database"
+mysql --local-infile=1 -u "$DB_USER" -p"$DB_PASS" -h "$DB_HOST" "$DB_NAME" <<EOF
+LOAD DATA LOCAL INFILE '$(pwd)/ppd_last_month.csv'
+INTO TABLE house_sales
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(transaction_id, price, transfer_date, postcode, property_type,
+ new_build, tenure, paon, street, record_status);
+EOF
